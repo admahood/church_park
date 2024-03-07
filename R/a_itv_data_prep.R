c3d92353519d89284d9e1e8a90f254e11e337924
Mahood <12001002933729@FEDIDCARD.GOV>
2024-03-07 13:15:48 -0700

    added ancillary data

---
+++ R/a_itv_data_prep.R
@@ -0,0 +1,132 @@
+# data prep
+
+library(tidyverse)
+library(janitor)
+library(lmerTest)
+
+# church park ingest SLA, Height=============
+if(!file.exists("data/church_traits.csv")){
+  ijc <- list.files("data/IJ_church_park/", full.names=T) %>%
+  lapply(FUN = read_csv)
+
+  lut_trt <- c("1A" = "m","1B" = "m","1C" = "bm","1D" = "b",
+               "2A" = "bm","2B" = "b","2C" = "m","2D" = "c",
+               "3A" = "m","3B" = "c","3C" = "bm","3D" = "b",
+               "4A" = "b","4B" = "bm","4C" = "c","4D" = "m",
+               "5A" = "b","5B" = "m","5C" = "c","5D" = "bm",
+               "6A" = "c","6B" = "b","6C" = "m","6D" = "bm")
+  
+  la_church <- ijc %>%
+    bind_rows() %>%
+    janitor::clean_names() %>%
+    tidyr::separate(label, into = c("plot", "species", "individual","ext", "end1", "end2")) %>%
+    dplyr::group_by(plot, species, individual) %>%
+    summarise(leaf_area_scanner = sum(area*percent_area/100)) %>%
+    ungroup() %>%
+    mutate(species_full = case_match(species, 
+                                     "CASP" ~ "Carex sp")) %>%
+    dplyr::select(-species)
+  
+  d_church_carex_sla <- readxl::read_xlsx("data/church_veg_trait_data.xlsx") |>
+    filter(trait == "SLA") %>%
+    mutate(species_full = case_match(species_code, 
+                                     "CASP" ~ "Carex sp",
+                                     "VASP" ~ "Vaccinium sp",
+                                     "SOSP" ~ "Oreochrysum parryi"), 
+           individual = as.character(individual)) %>%
+    dplyr::filter(species_full == "Carex sp") %>%
+    left_join(la_church, by = c("plot", "species_full", "individual")) %>%
+    dplyr::select(-date_yyyymmdd, -observed_by, -location, -leaf_area, -val_area,
+                  -real_val_area, -species_code, -trait, -wet_weight_g, -length_cm) %>%
+    mutate(lma = dry_weight_g/leaf_area_scanner,
+           sla = (leaf_area_scanner/dry_weight_g)/1000) |>
+    dplyr::select(plot, individual, lma, sla)
+  
+  d_church_carex <- readxl::read_xlsx("data/church_veg_trait_data.xlsx") |>
+    filter(trait == "LDMC") %>%
+    mutate(species_full = case_match(species_code, 
+                                     "CASP" ~ "Carex sp",
+                                     "VASP" ~ "Vaccinium sp",
+                                     "SOSP" ~ "Oreochrysum parryi"), 
+           individual = as.character(individual)) %>%
+    dplyr::filter(species_full == "Carex sp") %>%
+    dplyr::select(-date_yyyymmdd, -observed_by, -location, -leaf_area, -val_area,
+                  -real_val_area, -species_code, -trait, -entered_by) %>%
+    dplyr::rename(height_cm = length_cm) |>
+    mutate(height_cm = as.numeric(height_cm),
+           ldmc = (dry_weight_g*1000)/as.numeric(wet_weight_g)) %>%
+    dplyr::select(-wet_weight_g, -dry_weight_g) |>
+    left_join(d_church_carex_sla)
+  
+  d_church <- readxl::read_xlsx("data/church_veg_trait_data.xlsx",
+                                    col_types = c("date", "text", "text", 
+                                                  "text", "text", "text",
+                                                  "text", "numeric", "numeric",
+                                                  "numeric", "numeric", "numeric", "numeric",
+                                                  "text")) |> 
+    filter(trait == "all") %>%
+    mutate(species_full = case_match(species_code,
+                                     "VASP" ~ "Vaccinium sp",
+                                     "SOSP" ~ "Oreochrysum parryi"), 
+           individual = as.character(individual)) %>%
+    dplyr::select(-date_yyyymmdd, -observed_by, -location, -species_code, -trait, -entered_by) %>%
+    mutate(adjusted_la = leaf_area * lm(real_val_area ~ val_area, data = .)$coefficients[2] +
+             rnorm(sd = 0.0007076 * sqrt(nrow(.)), n = nrow(.))) %>% # drawn from the prior lm
+    dplyr::rename(height_cm = length_cm) %>%
+    mutate(lma = dry_weight_g/adjusted_la,
+           height_cm = height_cm |> as.numeric(),
+           sla = (adjusted_la/dry_weight_g)/1000, # something's off with the units... doesn't actually matter
+           ldmc = (dry_weight_g*1000)/wet_weight_g) %>%
+    dplyr::select(plot, individual, height_cm, sla, ldmc, species_full, lma) %>%
+    bind_rows(d_church_carex) %>%
+    mutate(trt = lut_trt[plot],
+           block = str_sub(plot, 1,1))
+  
+  write_csv(d_church, "data/church_traits.csv")}else{
+  d_church <- read_csv("data/church_traits.csv")
+}
+
+# church park visualization ====================================================
+
+p1 <- ggplot(d_church, aes(x = sla , fill = species_full)) +
+  geom_density(alpha=0.5) +
+  geom_rug(aes(color = species_full)) +
+  guides(color = "none")
+
+p2 <- ggplot(d_church, aes(x = ldmc , fill = species_full)) +
+  geom_density(alpha=0.5) +
+  geom_rug(aes(color = species_full)) +
+  guides(color = "none")
+
+p3 <- ggplot(d_church, aes(x = height_cm , fill = species_full)) +
+  geom_density(alpha=0.5) +
+  geom_rug(aes(color = species_full)) +
+  guides(color = "none")
+
+ggpubr::ggarrange(p1, p2, p3, nrow = 1, ncol=3, common.legend = TRUE) %>%
+  ggsave(plot = ., filename = "out/density_plots.png", width =9, height =5)
+
+ggplot(d_church, aes(x = height_cm, y=ldmc, color = species_full)) +
+  geom_point()
+ggplot(d_church, aes(x = height_cm, y=sla, color = species_full)) +
+  geom_point()
+ggplot(d_church, aes(x = sla, y=ldmc, color = species_full)) +
+  geom_point() +
+  ggtitle("LDMC tends to be inversely related to SLA and Leaf thickness (P-H 2013).",
+          "species with low LDMC tend to be associated with productive, often highly disturbed environments.")
+
+p1 <- ggplot(d_church, aes(x = trt, y=sla, fill = trt)) +
+  geom_boxplot() +
+  facet_wrap(~species_full, scales="free") +
+  theme(legend.position = "none");p1
+p2 <- ggplot(d_church, aes(x = trt, y=ldmc, fill = trt)) +
+  geom_boxplot() +
+  facet_wrap(~species_full, scales="free") +
+  theme(legend.position = "none")
+p3 <- ggplot(d_church, aes(x = trt, y=height_cm, fill = trt)) +
+  geom_boxplot() +
+  facet_wrap(~species_full, scales="free") +
+  theme(legend.position = "none")
+
+ggpubr::ggarrange(p1, p2, p3, nrow = 3, ncol=1, common.legend = TRUE) %>%
+  ggsave(plot = ., filename = "out/trait_x_treatment.png", width =7, height =10)
--- R/veg_analysis.R
+++ R/veg_analysis.R
@@ -26,7 +26,8 @@ comm <- comm_wide %>%
   dplyr::select(-block, -treatment) %>%
   as.data.frame() %>%
   tibble::column_to_rownames("row"); comm 
- 
+
+# nmds ===========
 nmds <- comm %>%
   wisconsin() %>%
   metaMDS()
@@ -44,18 +45,86 @@ species <- as.data.frame(cbind(sp, p=ef$vectors$pvals)) %>%
   tibble::rownames_to_column("species")  %>%
   filter(p < 0.05)
 
+adab <- adonis2(comm ~ treatment, data = sites)
 
 p_ab <- ggplot(site_scores, aes(x=NMDS1, y=NMDS2)) +
   coord_fixed() +
-  geom_point(size=2, aes(color = treatment)) +
+  geom_point(size=4, aes(color = treatment)) +
   geom_segment(data = species,x=0,y=0, color = "grey",arrow = arrow(),
                aes(yend = NMDS2, xend = NMDS1), lwd=1)+
-  geom_text(data = species,size=4, aes(label = species), color = "grey40") +
+  ggrepel::geom_text_repel(data = species,size=4, aes(label = species), color = "grey40") +
   theme_classic() +
   theme(panel.background = element_rect(fill="transparent", color = "black")) +
-  ggtitle("Abundance-Based");p_ab
+  ggtitle("Abundance-Based", paste("Treatement explaines", 
+                                   signif(adab$R2[1], 3) * 100, 
+                                   "% of the variation"));p_ab
 
-adonis2(comm ~ treatment, data = sites)
 
 
+# occurrence based ====================
 
+nmds <- comm %>%
+  decostand(method = "pa") %>%
+  metaMDS()
+
+stressplot(nmds)
+
+site_scores <- as.data.frame(vegan::scores(nmds)$sites) %>%
+  as_tibble(rownames = "plot") %>%
+  left_join(sites); site_scores
+
+ef <- envfit(nmds, comm %>%
+               decostand(method = "pa"), na.rm = T, permutations = 9999)
+eft <- envfit(nmds, sites[,2], na.rm = T, permutations = 9999)
+sp <-as.data.frame(ef$vectors$arrows*sqrt(ef$vectors$r))
+species <- as.data.frame(cbind(sp, p=ef$vectors$pvals)) %>%
+  tibble::rownames_to_column("species")  %>%
+  filter(p < 0.01)
+
+adoc <- adonis2(comm %>% decostand("pa") ~ treatment, data = sites)
+
+p_oc <- ggplot(site_scores, aes(x=NMDS1, y=NMDS2)) +
+  coord_fixed() +
+  geom_point(size=4, aes(color = treatment)) +
+  geom_segment(data = species,x=0,y=0, color = "grey",arrow = arrow(),
+               aes(yend = NMDS2, xend = NMDS1), lwd=1)+
+  ggrepel::geom_text_repel(data = species,size=4, aes(label = species), color = "grey40") +
+  theme_classic() +
+  theme(panel.background = element_rect(fill="transparent", color = "black")) +
+  ggtitle("Occurrence-Based", paste("Treatement explaines", 
+                                    signif(adoc$R2[1], 3) * 100, 
+                                    "% of the variation"));p_oc
+
+
+ggpubr::ggarrange(p_ab, p_oc, common.legend = TRUE, widths = c(1,1.15)) %>%
+  ggsave(width =10, height =6, filename = "out/nmds.png", plot = ., bg="white")
+
+
+# biodiversity indexes
+
+diversity(comm, groups = sites$treatment)
+df <- data.frame(turnover = NA, nestedness = NA, jaccard = NA)
+df[1,] <- nestedbetajac(comm[sites$treatment == "b"])
+df[2,] <- nestedbetajac(comm[sites$treatment == "bm"])
+df[3,] <- nestedbetajac(comm[sites$treatment == "c"])
+df[4,] <- nestedbetajac(comm[sites$treatment == "m"])
+df$trt <- c("b", "bm", "c", "m")
+
+ggplot(df, aes(x = turnover, y=trt)) +
+  geom_bar(stat = "identity")
+ggplot(df, aes(x = nestedness, y=trt)) +
+  geom_bar(stat = "identity")
+ggplot(df, aes(x = jaccard, y=trt)) +
+  geom_bar(stat = "identity")
+
+pdiv <- sites %>%
+ mutate(shannon = diversity(comm, index = "shannon"),
+        simpson = diversity(comm, index = "simpson"),
+        invsimpson = diversity(comm, index = "invsimpson"),
+        richness = vegan::specnumber(comm)) %>%
+  pivot_longer(cols = c("shannon", "simpson", "invsimpson", "richness")) %>%
+  ggplot(aes(x=treatment, y=value, fill = treatment)) +
+  geom_boxplot() +
+  facet_wrap(~name, scales = "free_y") ;pdiv
+
+ggsave(plot = pdiv, filename = "out/alphadiversity_x_treatment.png", width =5, height = 5)
